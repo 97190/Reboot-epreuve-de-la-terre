ed6abccdce8c625fce0d6acb8a3b72a01c5e5a60
erika mayoute <e.mayoute44@gmail.com>
2022-01-28 10:10:43 +0100

    pair

--- pair_ou_impair.rb
+++ pair_ou_impair.rb
@@ -1,7 +1,22 @@
 n = ARGV[0].to_i
+if n.to_s
+  puts "Tu ne me la mettras pas à l'envers."
+elsif n.class == NilClass
+  puts "Tu ne me la mettras pas à l'envers."
+  exit
+end
 
-if n % 2 == 0
+if n.to_i % 2 == 0
   puts "pair"
     else
   puts "impair"
 end
+
+puts "#{n}"
+# n = ARGV[0].to_i
+
+# if n % 2 == 0
+#   puts "pair"
+#     else
+#   puts "impair"
+# end
