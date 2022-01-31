d15f8ba9d0570701548ad0c906ab2a80fddd344d
erika mayoute <e.mayoute44@gmail.com>
2022-01-31 15:43:38 +0100

    12_to_24

--- 12_to_24.rb
+++ 12_to_24.rb
@@ -4,15 +4,22 @@
 # $> ruby exo.rb 11:40PM
 # 23:40
 # Attention : midi et minuit.
-
+# 11:40PM
 tab = ARGV[0].split(":") # Sépare les heures et les minutes données en arguments.
+# ["11", "40PM"]
 heure = tab[0] # Affiche 11 en string
+# "11"
 minutes = tab[1] # Affiche 40 en string
+# "40PM"
 
-if heure.to_i == 12 #Si mon heure donnée en argumnt est 12 je passe à l'instruction suivante
-  puts "#{heure.to_i - 12}:#{minutes} de minuit"
-elsif (heure.to_i >= 1 && heure.to_i <= 11 ) #Si mon heure donnée en argument, varie dans la fourchette entre 1 et 11 passe à l'instruction suivante
-  puts "#{heure.to_i + 12}:#{minutes}"
+if heure == "12" && minutes.include?("AM")
+  puts "#{heure.to_i - 12}:#{minutes.chop().chop()}"
+elsif (heure.to_i >= 1 &&  heure.to_i <= 11  && minutes.include?("AM"))
+  puts "#{heure}:#{minutes.chop().chop()}"
+elsif heure == "12" && minutes.include?("PM")
+  puts "#{heure}:#{minutes.chop().chop()}"
+elsif (heure.to_i >= 1 && heure.to_i <= 11 && minutes.include?("PM"))
+  puts "#{heure.to_i + 12}:#{minutes.chop().chop()}"
 end
 
 
@@ -31,6 +38,7 @@ end
 
 #Groupe 24h00 =  Groupe 12h00
 # 00:00 = 12:00 AM (minuit) #groupe minuit AM
+
 # 01:00 = 1:00 AM (une heure du matin) #groupe AM
 # 02:00 = 2:00 AM.
 # 03:00 = 3:00 AM.
@@ -42,7 +50,8 @@ end
 # 09:00 = 9:00 AM.
 # 10:00 = 10:00 AM.
 # 11:00 = 11:00 AM.
-# 12:00 = 12:00 PM (midi).#groupe midi PM # N'arrive pas à convertir
+
+# 12:00 = 12:00 PM (midi)
 # 13:00 = 1:00 PM.(une heure de l’après-midi)# groupe PM
 # 14:00 = 2:00 PM.
 # 15:00 = 3:00 PM.
