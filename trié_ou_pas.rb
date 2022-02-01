f401172f45026cb8b9a70060dbe474f5d9e3081c
erika mayoute <e.mayoute44@gmail.com>
2022-02-01 18:29:35 +0100

    trié_ou_pas

--- trié_ou_pas.rb
+++ trié_ou_pas.rb
@@ -7,4 +7,43 @@
 # $> ruby exo.rb “Salut”
 # erreur.
 
-# puts ARGV[0].to_i
+# Recueillir les ARGV données en entrée quelque soit le type de départ (string, integer)
+
+list = [3, 8, 9, 12]
+letter = ARGV.to_s
+
+# Affiché un retour "pas triée !" quleque soit la donnée en entré"
+
+if list
+  puts "Pas triée !"
+elsif letter
+  puts "erreur."
+  exit
+end
+# number = [ARGV[0].to_i, ARGV[1].to_i, ARGV[2].to_i, ARGV[3].to_i]
+# if ARGV.to_s
+# puts 'erreur.'
+# exit
+# end
+
+# puts liste = [..20]
+# liste.sort do |x, y, z|
+#   puts "triée"
+# end
+# array = [2, 3, 13, 7, 8, 9, 11, 1, 5]
+# def bubble_sort(array)
+#   length = array.length
+#   (length - 1).times do |round|
+#     (length - 1 - round).times do |n|
+#       if array[length - n - 1] < array[length - n - 2]
+#         smaller = array[length - n - 1]
+#         array[length - n - 1] = array[length - n - 2]
+#         array[length - n - 2] = smaller
+#       end
+#     end
+#   end
+#   array
+# end
+
+
+# puts bubble_sort(array) #=> [1, 2, 3, 5, 7, 8, 9, 11, 13]
