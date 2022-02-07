65bd58dc2f3c2fc3a80d8c9cbdde8bad3e9082e0
erika mayoute <e.mayoute44@gmail.com>
2022-02-07 12:21:01 +0100

    terre_finish

--- terre_finish.rb
+++ terre_finish.rb
@@ -1,3 +1,1 @@
-# puts "J'ai pas terminer l'epreuve de la terre et c'était compliqué."
-puts Array 001..10
-# Affiche une sé
+puts "J'ai terminer l'epreuve de la terre et c'était [compliqué nerveusement]."
